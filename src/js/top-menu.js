0f4ae4e32112b475b457fb1e9a2df09122900501
kiet <mkla1252006@gmail.com>
2026-04-24 14:44:18 +0700

    xong home

--- src/js/top-menu.js
+++ src/js/top-menu.js
@@ -1,22 +1,26 @@
-const topMenu = document.getElementById("ct-top-menu");
-const menuIcon = document.getElementById("ct-top-menu-icon");
+document.addEventListener("DOMContentLoaded", () => {
+  const topMenu = document.getElementById("ct-top-menu");
+  const menuIcon = document.getElementById("ct-top-menu-icon");
 
-// Bắt sự kiện click trên toàn bộ trang web
-document.addEventListener("click", (e) => {
-  //  Khách click vào cái Nút Icon (3 gạch)
-  if (menuIcon.contains(e.target)) {
-    topMenu.classList.toggle("hidden");
-    topMenu.classList.toggle("ct-topmenu-expanded");
-  }
-  //  Khách click ra ngoài
-  else {
-    // Điều kiện: Menu ĐANG MỞ và Khách KHÔNG click vào chính cái Menu
-    if (
-      topMenu.classList.contains("ct-topmenu-expanded") &&
-      !topMenu.contains(e.target)
-    ) {
-      topMenu.classList.add("hidden");
-      topMenu.classList.remove("ct-topmenu-expanded");
+  // Bắt sự kiện click trên toàn bộ trang web
+  document.addEventListener("click", (e) => {
+    // 1. Khách click vào cái Nút Icon (3 gạch)
+    if (menuIcon.contains(e.target)) {
+      topMenu.classList.toggle("opacity-0");
+      topMenu.classList.toggle("invisible");
+      topMenu.classList.toggle("-translate-y-5");
     }
-  }
+    // 2. Khách click ra ngoài
+    else {
+      // Điều kiện: Menu ĐANG MỞ (nghĩa là nó không bị tàng hình)
+      // và Khách KHÔNG click vào chính nội dung của cái Menu
+      if (
+        !topMenu.classList.contains("invisible") &&
+        !topMenu.contains(e.target)
+      ) {
+        // Đóng menu lại: Add lại 3 class ẩn
+        topMenu.classList.add("opacity-0", "invisible", "-translate-y-5");
+      }
+    }
+  });
 });
